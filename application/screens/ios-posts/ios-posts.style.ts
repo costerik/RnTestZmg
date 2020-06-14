8034d6e373e18a30bc7a93b03d7e8714820b7e82
Erik Ahumada <costerik@hotmail.com>
2020-06-13 23:39:43 -0500

    segmented control added

--- application/screens/ios-posts/ios-posts.style.ts
+++ application/screens/ios-posts/ios-posts.style.ts
@@ -4,4 +4,12 @@ export default StyleSheet.create({
   container: {
     flex: 1,
   },
+  activeFontStyle: {color: '#ffffff'},
+  segmentedControlStyle: {
+    backgroundColor: '#ffffff',
+    borderColor: 'green',
+    borderWidth: 2,
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
 });
--- application/screens/ios-posts/ios-posts.tsx
+++ application/screens/ios-posts/ios-posts.tsx
@@ -1,5 +1,6 @@
 import React, {ReactElement} from 'react';
 import {Text, View} from 'react-native';
+import SegmentedControl from '@react-native-community/segmented-control';
 
 // types
 import type {IOSPostsType} from './ios-posts.types';
@@ -8,8 +9,19 @@ import type {IOSPostsType} from './ios-posts.types';
 import style from './ios-posts.style';
 
 const IOSPosts = ({}: IOSPostsType): ReactElement => {
+  const [filter, setFilter] = React.useState(0);
   return (
     <View style={style.container}>
+      <SegmentedControl
+        values={['One', 'Two']}
+        selectedIndex={filter}
+        onChange={(event): void => {
+          setFilter(event.nativeEvent.selectedSegmentIndex);
+        }}
+        tintColor="green"
+        activeFontStyle={style.activeFontStyle}
+        style={style.segmentedControlStyle}
+      />
       <Text>IOS Posts</Text>
     </View>
   );
